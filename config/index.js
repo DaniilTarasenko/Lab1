1875a686cf409ffac949d907ffc993a162e3d72d
Daniil Tarasenko <80252099+DaniilTarasenko@users.noreply.github.com>
2024-04-29 15:21:49 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,20 +1,19 @@
-// файл ./config/index.js
-const fs = require('fs');
-
+// файл ./backend/config/index.js
 const config = {
-	db: {
+	db : {
     mysql : {
-      host: 'localhost',
-      user: 'user', // замените на своего пользователя
-      database: 'appdb', // можете заменить 'appdb' на свое название базы данных
-      password: 'yourPasswordHere', // замените это на пароль от своего пользователя
-      port: 3000, // порт базы данных
+      host     : 'localhost',
+      user     : 'user', // замените на своего пользователя
+      database : 'appdb', // можете заменить 'appdb' на свое название базы данных
+      password : 'yourPasswordHere' // замените это на пароль от своего пользователя
+      port: 25060, // порт базы данных
 			ssl: {
-			  ca: fs.readFileSync('/lab3/ca-certificate-test.crt'), // Путь к файлу ca.crt
+			  ca: fs.readFileSync('path/ca-certificate-test.crt'), // Путь к файлу ca.crt
 			}
     },
   }, 
-  port: 3000 // порт на котором будет запущен сервер приложения
+  port : 3000 // порт на котором будет запущен сервер приложения
+  jwtSecret: 'mysecretkey' // Секретный ключ для подписи и верификации JWT токенов, вы его либо сами генерируете, либо сами придумываете
 };
 
 module.exports =  config;
