04f6ff5ca5d1c63e8b7a82267802e862923e874e
Daniil Tarasenko <80252099+DaniilTarasenko@users.noreply.github.com>
2024-05-04 15:36:57 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
-/ файл ./app.js
+// файл ./app.js
 const express = require('express');
-const mysql = require('mysql');
+const { Pool } = require('pg');
 const config = require('/lab3/index.js');
 const cors = require('cors');
 const app = express();
@@ -10,29 +10,33 @@ app.use(express.json());
 app.use(cors());
 
 // Конфигурация подключения к базе данных
-const dbConnection = mysql.createConnection(config.db.mysql);
+const pool = new Pool(config.db.postgresql);
 
-// Подключение к базе данных
-dbConnection.connect((err) => {
-  if (err) {
-    console.error('Ошибка подключения к базе данных: ' + err.stack);
-    return;
-  }
-  console.log('Подключение к базе данных успешно установлено');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 // Пример маршрута Express
 app.get('/getTasks', (req, res) => {
   // Пример запроса к базе данных
-  dbConnection.query('SELECT * FROM tasks', (err, results) => {
+  pool.query('SELECT * FROM tasks', (err, results) => {
     if (err) {
       console.error('Ошибка выполнения запроса: ' + err.stack);
       res.status(500).send('Ошибка сервера');
       return;
     }
-    console.log('Результаты запроса:', results);
-    res.json(results);
+    console.log('Результаты запроса:', results.rows);
+    res.json(results.rows);
   });
+});
+
+// Подключение к базе данных
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Ошибка подключения к базе данных: ' + err.stack);
+    return;
+  }
+  console.log('Подключение к базе данных успешно установлено');
 });
 
 // Пример маршрута Express для добавления записи в таблицу tasks с указанным именем
@@ -46,7 +50,6 @@ app.post('/addTask', (req, res) => {
     res.status(400).send('Не указано имя задачи');
     return;
   }
-});
 
 app.get('/createTasksTable', (req, res) => {
   const createTableSql = 'CREATE TABLE IF NOT EXISTS tasks (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)';
@@ -60,61 +63,81 @@ app.get('/createTasksTable', (req, res) => {
     res.send('Таблица tasks успешно создана или уже существует');
   });
 });
+app.post()
 
-// Нахождение задачи по id
-app.get('/tasks/:id', (req, res) => {
-  const taskId = req.params.id; // Получаем id задачи из URL
-  const findTaskSql = 'SELECT * FROM tasks WHERE id = ?'; // SQL запрос для поиска задачи по id
-  dbConnection.query(findTaskSql, [taskId], (err, result) => {
-    if (err){
-      console.error('Ошибка при выполнении запроса:'+ err.stack);
-      res.status(500).send('Ошибка сервера при выполнении запроса');
-      return;
-    }
-    if (result.length > 0) {
-      res.send(result[0]); // Отправляем найденную задачу в качестве ответа
-    } else {
-      res.status(404).send('Задача с указанным id не найдена');
-    }
-  });
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // Хеширование пароля
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Сохранение пользователя в базе данных
+    dbConnection.query(
+      `INSERT INTO users (username, password) VALUES ('${username}', '${hashedPassword}')`,
+      (err, result) => {
+        if (err) {
+          console.error('Ошибка выполнения запроса: ' + err.stack);
+          res.status(500).send('Ошибка сервера');
+          return;
+        }
+        console.log('Пользователь успешно зарегистрирован');
+        res.status(201).send('Пользователь успешно зарегистрирован');
+      }
+    );
+  } catch (error) {
+    console.error('Ошибка при регистрации пользователя:', error);
+    res.status(500).send('Ошибка сервера');
+  }
 });
 
-// Редактирование задачи по id
-app.put('/tasks/:id', (req, res) => {
-  const taskId = req.params.id; // Получаем id задачи из URL
-  const { name, description, folderId } = req.body; // Получаем новые данные для задачи из тела запроса
-  const updateTaskSql = 'UPDATE tasks SET name = ?, description = ?, folderId = ? WHERE id = ?'; // SQL запрос для редактирования задачи
-  dbConnection.query(updateTaskSql, [name, description, folderId, taskId], (err, result) => {
-    if (err){
-      console.error('Ошибка при выполнении запроса:'+ err.stack);
-      res.status(500).send('Ошибка сервера при выполнении запроса');
-      return;
-    }
-    if (result.affectedRows > 0) {
-      res.send('Задача успешно отредактирована');
-    } else {
-      res.status(404).send('Задача с указанным id не найдена');
-    }
-  });
+// Вход пользователя
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // Поиск пользователя в базе данных
+    dbConnection.query(
+      `SELECT * FROM users WHERE username = '${username}'`,
+      async (err, results) => {
+        if (err) {
+          console.error('Ошибка выполнения запроса: ' + err.stack);
+          res.status(500).send('Ошибка сервера');
+          return;
+        }
+        if (results.length === 0) {
+          res.status(401).send('Неверные учетные данные');
+          return;
+        }
+        const user = results[0];
+        // Проверка пароля
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+          res.status(401).send('Неверные учетные данные');
+          return;
+        }
+        // Генерация JWT токена
+        const token = jwt.sign({ username: user.username }, config.jwtSecret);
+        res.status(200).json({ token });
+      }
+    );
+  } catch (error) {
+    console.error('Ошибка при входе пользователя:', error);
+    res.status(500).send('Ошибка сервера');
+  }
 });
 
-// Удаление задачи по id
-app.delete('/tasks/:id', (req, res) => {
-  const taskId = req.params.id; // Получаем id задачи из URL
-  const deleteTaskSql = 'DELETE FROM tasks WHERE id = ?'; // SQL запрос для удаления задачи
-  dbConnection.query(deleteTaskSql, [taskId], (err, result) => {
-    if (err){
-      console.error('Ошибка при выполнении запроса:'+ err.stack);
-      res.status(500).send('Ошибка сервера при выполнении запроса');
-      return;
-    }
-    if (result.affectedRows > 0) {
-      res.send(`Задача с id ${taskId} успешно удалена`);
-    } else {
-      res.status(404).send('Задача с указанным id не найдена');
-    }
-  });
+// Проверка аутентификации с использованием JWT
+app.get('/profile', (req, res) => {
+  // Получение токена из заголовка Authorization
+  const token = req.headers.authorization.split(' ')[1];
+  try {
+    // Проверка токена
+    const decoded = jwt.verify(token, config.jwtSecret);
+    res.status(200).json({ username: decoded.username });
+  } catch (error) {
+    console.error('Ошибка при проверке токена:', error);
+    res.status(401).send('Неверный токен');
+  }
 });
+
 
   // SQL-запрос для добавления записи с указанным именем в таблицу tasks
   const sqlQuery = `INSERT INTO tasks (name) VALUES ('${taskName}')`;
